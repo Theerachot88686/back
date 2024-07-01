09c4c1d13ea4518c1f4d710b66e483e8ed0bf8f7
theerachot <theerachot.la@rmuti.ac.th>
2024-07-01 20:46:58 +0700

    first co

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Database connected!'))
   .catch(err => console.error('Database connection error:', err));
 
